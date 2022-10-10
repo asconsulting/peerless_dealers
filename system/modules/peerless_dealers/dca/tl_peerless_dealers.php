45c5f44234f7999e177b990041a6a7fcb852d102
Andrew Stevens <andrew@andrewstevens.consulting>
2022-10-10 13:45:08 -0400

    Delete Confirm

--- system/modules/peerless_dealers/dca/tl_peerless_dealers.php
+++ system/modules/peerless_dealers/dca/tl_peerless_dealers.php
@@ -79,7 +79,7 @@ $GLOBALS['TL_DCA']['tl_peerless_dealers'] = array
                 'label'               => &$GLOBALS['TL_LANG']['tl_peerless_dealers']['delete'],
                 'href'                => 'act=delete',
                 'icon'                => 'delete.gif',
-                'attributes'          => 'onclick="if(!confirm(\'' . $GLOBALS['TL_LANG']['MSC']['deleteConfirm'] . '\'))return false;Backend.getScrollOffset()"'
+                'attributes'          => 'onclick="if(!confirm(\'' . ($GLOBALS['TL_LANG']['MSC']['deleteConfirm'] ?? null) . '\'))return false;Backend.getScrollOffset()"'
             ),
             'toggle' => array
 			(
